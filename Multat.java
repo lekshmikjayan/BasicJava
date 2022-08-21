4e6de3ebfbe8ea72a922b2fd2d5e440aacdee501
Lekshmi K Jayan <lekshmikjayan27@gmail.com>
2022-08-21 18:34:27 +0530

    completed

---
+++ Multat.java
@@ -0,0 +1,11 @@
+public class Multat{
+    String fname = "yoyo";
+    int age = 45;
+
+    public static void main (String[] args){
+        Multat obj = new Multat ();
+        //obj.fname = "helen";
+       // obj.age = 68;
+        System.out.println("My name is : " + obj.fname + " and I am "+ obj.age + " years old");
+    }
+} 
--- Objt.java
+++ Objt.java
@@ -1,12 +1,13 @@
 public class Objt{
     int id = 2;
-id = 7;
     public static void main (String [] args){
         Objt xyz = new Objt();
-        Objt pqr = new Objt();
+       // xyz.id = 47;
+      Objt pqr = new Objt();
+      pqr.id = 39;
       
         System.out.println(xyz.id);
-        System.out.println(pqr.id);
+      System.out.println(pqr.id);
         
       //  System.out.println(pqr.id);
 
